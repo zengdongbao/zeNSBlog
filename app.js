a9e1810ccf49cdc3b2ee8d20cc0b4fe92ae53473
zen <zen@yyq.cn>
2013-05-08 16:24:42 +0800

    test

--- app.js
+++ app.js
@@ -30,6 +30,11 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
+
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+
+
+//module.exports = http.createServer(app);
